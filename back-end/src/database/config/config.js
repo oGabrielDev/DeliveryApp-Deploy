8fbb392cefc0058bea352d9f1205406d7d43ff8a
oGabrielDev <gabriel.thiago611@gmail.com>
2023-03-28 17:24:03 -0300

    feat: Config para utilizar o postgres

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,17 +11,15 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  dialect: 'postgres',
+  define: {
+    timestamp: true,
+  }
 };
 
 module.exports = {
