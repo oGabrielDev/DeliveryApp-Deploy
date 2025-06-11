7b378cc1a225b962a07101dbbc85889723b03e3e
Gabriel thiago <gabriel.thiago611@gmail.com>
2025-06-10 21:39:43 -0300

    atualizacao railway

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
 
 module.exports = {
-  url: process.env.DATABASE_URL,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
+  test: {
+    ...development
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
