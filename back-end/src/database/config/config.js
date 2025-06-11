1ae1ca3ace063b1babc68111616c0f56d243260c
Gabriel thiago <gabriel.thiago611@gmail.com>
2025-06-10 21:28:30 -0300

    atualizacao config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,35 +1,12 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
-const options = {
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  dialect: 'postgres',
-  define: {
-    timestamp: true,
-  }
-};
-
 module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-  production: {
-    ...options,
+  url: process.env.DATABASE_URL,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 };
